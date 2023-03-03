ed1868e221f0fe8bed1344d1382d51e5fa0d3896
Saulo Costa <saulotarsobc@gmail.com>
2023-03-03 14:55:28 -0300

    :fire:

--- js/home.js
+++ js/home.js
@@ -6,6 +6,7 @@ const ytdl = require('ytdl-core');
 /* html elements */
 const url = document.getElementById('url');
 const buscar = document.getElementById('buscar');
+const thumb = document.getElementById('thumb');
 const title = document.getElementById('title');
 const description = document.getElementById('description');
 /* html elements */
@@ -14,11 +15,20 @@ buscar.addEventListener('click', async () => {
     buscarVideo();
 });
 
-async function buscarVideo(){
+async function buscarVideo() {
     let info = await ytdl.getInfo(url.value);
-    console.log(info.videoDetails);
+    console.log(info);
     title.value = info.videoDetails.title;
     description.value = info.videoDetails.description;
+    thumb.src = info.videoDetails.thumbnails[info.videoDetails.thumbnails.length - 1].url;
 };
 
+function cleanAll(){
+    thumb.src = "./image/nothing.png";
+    title.value = "";
+    description.value = "";
+}
+
 buscarVideo();
+
+console.log(__dirname);
