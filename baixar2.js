505b4de28e2a81bde3043acdb01d49df292e0305
Saulo Costa <saulotarsobc@gmail.com>
2023-03-04 15:35:11 -0300

    :fire:

--- baixar2.js
+++ baixar2.js
@@ -5,13 +5,10 @@ const videoId = '29bWdogk2tA'; // Insira o ID do vídeo que você deseja baixar 
 const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
 
 ytdl.getInfo(videoUrl).then(info => {
-
     const { formats } = info;
-
     const { title } = info.videoDetails;
     const format = formats[13];
     const container = format.container
-
     // Baixe o vídeo
     const stream = ytdl.downloadFromInfo(info, { format });
     let downloaded = 0;
@@ -22,6 +19,5 @@ ytdl.getInfo(videoUrl).then(info => {
         const totalMB = totalLength / (1024 * 1024);
         console.log(`Baixando "${title}.${container}" - ${progress.toFixed(2)}% concluído (${downloadedMB.toFixed(2)} MB de ${totalMB.toFixed(2)} MB)`);
     });
-
     stream.pipe(fs.createWriteStream(`${title}.${container}`));
 });
